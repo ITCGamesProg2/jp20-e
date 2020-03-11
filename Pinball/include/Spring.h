682a6feb6e1d54c68fada55e502eb7144a095441
Ben Millar <c00236772@itcarlow.ie>
2020-03-11 15:27:18 +0000

    Implemented spring shape, and bounding box update

--- Pinball/include/Spring.h
+++ Pinball/include/Spring.h
@@ -8,16 +8,46 @@ public:
 	Spring();
 	~Spring() = default;
 
+	/// <summary>
+	/// @brief Updates the spring along with the game loop
+	/// </summary>
+	/// <param name="t_dTime">time since last update</param>
+	void update(sf::Time t_dTime);
+
+	/// <summary>
+	/// @brief Returns the spring shape
+	/// </summary>
+	/// <param name="t_window"></param>
+	sf::RectangleShape getShape();
+
+
+	/// @brief Sets the position of the spring
+	/// </summary>
+	/// <param name="t_pos"></param>
 	void setPosition(sf::Vector2f t_pos) override;
+
+	/// <summary>
+	/// @brief Returns the position of the spring
+	/// </summary>
+	/// <returns></returns>
 	const sf::Vector2f getPosition() const override;
 
+	/// <summary>
+	/// @brief Returns the bounds of our line
+	/// </summary>
+	/// <returns></returns>
 	Line const& getBounds() const  override;
 private:
 
 	void setupSprite();
 	void setupBoundingBox();
 
+	void updateBoundingBox();
+
 	sf::RectangleShape m_body;
+
+	float m_width;
+	float m_height;
 
 	Line m_boundingBox;
 };
--- Pinball/src/Spring.cpp
+++ Pinball/src/Spring.cpp
@@ -1,7 +1,49 @@
 #include "Spring.h"
 
-Spring::Spring()
+Spring::Spring() :
+	m_width{50.0f},
+	m_height{10.0f}
 {
+	setupSprite();
+	setupBoundingBox();
+}
+
+///////////////////////////////////////////////////////////////
+
+void Spring::setupSprite()
+{
+	m_body.setFillColor(sf::Color::Black);
+	m_body.setSize({ m_width, m_height });
+}
+
+///////////////////////////////////////////////////////////////
+
+void Spring::setupBoundingBox()
+{
+	m_boundingBox.p1 = m_position;
+	m_boundingBox.p2 = m_position + sf::Vector2f{ m_width, 0.0f };
+}
+
+///////////////////////////////////////////////////////////////
+
+void Spring::update(sf::Time t_dTime)
+{
+	updateBoundingBox();
+}
+
+///////////////////////////////////////////////////////////////
+
+void Spring::updateBoundingBox()
+{
+	m_boundingBox.p1 = m_position;
+	m_boundingBox.p2 = m_position + sf::Vector2f{ m_width, 0.0f };
+}
+
+///////////////////////////////////////////////////////////////
+
+sf::RectangleShape Spring::getShape()
+{
+	return m_body;
 }
 
 ///////////////////////////////////////////////////////////////
@@ -23,16 +65,4 @@ const sf::Vector2f Spring::getPosition() const
 Line const& Spring::getBounds() const
 {
 	return m_boundingBox;
-}
-
-///////////////////////////////////////////////////////////////
-
-void Spring::setupSprite()
-{
-}
-
-///////////////////////////////////////////////////////////////
-
-void Spring::setupBoundingBox()
-{
 }
