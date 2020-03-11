cc28863834bc0aeb98357d54911c813aa008c966
(Student) - Szymon Piskorz <c00242808@ITCARLOW.IE>
2020-03-11 15:18:32 +0000

    Added all needed Data structs and basic empty yaml level

--- Pinball/include/LevelLoader.h
+++ Pinball/include/LevelLoader.h
@@ -33,11 +33,45 @@ struct BackgroundData
 	std::string m_fileName;
 };
 
+/// <summary>
+/// @brief A struct to represent the Left Flipper data in the level 
+/// </summary>
+struct LeftFlipperData
+{
+	sf::Vector2f m_position;
+};
+
+/// <summary>
+/// @brief A struct to represent the Left Flipper data in the level 
+/// </summary>
+struct RightFlipperData
+{
+	sf::Vector2f m_position;
+};
+
+/// <summary>
+/// @brief a struct to hold the positions of the barrier
+/// </summary>
+struct BarrierData
+{
+	sf::Vector2f m_position1;
+	sf::Vector2f m_position2;
+};
+
+struct SpringData
+{
+	sf::Vector2f m_position;
+};
+
 struct LevelData
 {
 	BackgroundData m_background;
 	BallData m_ball;
 	std::vector<ObstacleData> m_obstacles;
+	LeftFlipperData m_leftFlipper;
+	RightFlipperData m_rightFlipper;
+	std::vector<BarrierData> m_barriers;
+	SpringData m_spring;
 };
 
 class LevelLoader
--- Pinball/src/LevelLoader.cpp
+++ Pinball/src/LevelLoader.cpp
@@ -8,6 +8,14 @@ void operator >> (const YAML::Node& obstacleNode, ObstacleData& obstacle)
 	obstacle.m_position.y = obstacleNode["position"]["y"].as<float>();
 }
 
+void operator >> (const YAML::Node& barrierNode, BarrierData& barrier)
+{
+	barrier.m_position1.x = barrierNode["start"]["x"].as<float>();
+	barrier.m_position1.y = barrierNode["start"]["y"].as<float>();
+	barrier.m_position2.x = barrierNode["end"]["x"].as<float>();
+	barrier.m_position2.y = barrierNode["end"]["y"].as<float>();
+}
+
 void  operator >> (const YAML::Node& backgroundNode, BackgroundData& background)
 {
 	background.m_fileName = backgroundNode["file"].as<std::string>();
@@ -19,12 +27,35 @@ void operator >> (const YAML::Node& ballNode, BallData& ball)
 	ball.m_position.y = ballNode["position"]["y"].as<float>();
 }
 
+void operator >> (const YAML::Node& leftFlipperNode, LeftFlipperData& flipper)
+{
+	flipper.m_position.x = leftFlipperNode["position"]["x"].as<float>();
+	flipper.m_position.y = leftFlipperNode["position"]["y"].as<float>();
+}
+
+void operator >> (const YAML::Node& rightFlipperNode, RightFlipperData& flipper)
+{
+	flipper.m_position.x = rightFlipperNode["position"]["x"].as<float>();
+	flipper.m_position.y = rightFlipperNode["position"]["y"].as<float>();
+}
+
+void operator >> (const YAML::Node& springNode, SpringData& spring)
+{
+	spring.m_position.x = springNode["position"]["x"].as<float>();
+	spring.m_position.y = springNode["position"]["y"].as<float>();
+}
 
 void operator >> (const YAML::Node& levelNode, LevelData& level)
 {
 	levelNode["background"] >> level.m_background;
 
 	levelNode["ball"] >> level.m_ball;
+
+	levelNode["leftFlipper"] >> level.m_leftFlipper;
+
+	levelNode["rightFlipper"] >> level.m_rightFlipper;
+
+	levelNode["spring"] >> level.m_spring;
 
 	const YAML::Node& obstaclesNode = levelNode["obstacles"].as<YAML::Node>();
 	{
@@ -33,6 +64,16 @@ void operator >> (const YAML::Node& levelNode, LevelData& level)
 			ObstacleData obstacle;
 			obstaclesNode[i] >> obstacle;
 			level.m_obstacles.push_back(obstacle);
+		}
+	}
+
+	const YAML::Node& barrierNode = levelNode["barriers"].as<YAML::Node>();
+	{
+		for (unsigned i = 0; i < barrierNode.size(); i++)
+		{
+			BarrierData barrier;
+			barrierNode[i] >> barrier;
+			level.m_barriers.push_back(barrier);
 		}
 	}
 
@@ -51,12 +92,12 @@ void LevelLoader::load(int nr, LevelData& level)
 	try
 	{
 		YAML::Node baseNode = YAML::LoadFile(ss.str());
-		/*if (baseNode.IsNull())
+		if (baseNode.IsNull())
 		{
 			std::string message("File: " + ss.str() + " not found");
 			throw std::exception(message.c_str());
-		}*/
-		//baseNode >> level;
+		}
+		baseNode >> level;
 	}
 	catch (YAML::ParserException& e)
 	{
